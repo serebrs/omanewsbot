5306504a0345042a23e514386010e7c251021f9c
serebrs <serebr.s@gmail.com>
2021-10-03 13:36:57 +0600

    remove Heroku MySQL

--- dbpool.js
+++ dbpool.js
@@ -1,11 +1,11 @@
 const mariadb = require('mariadb');
 
-exports.pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_DB,
-    acquireTimeout: 1000
-});
-
-exports.poolHeroku = mariadb.createPool(process.env.MARIA_CLEARDB);
+module.exports = {
+    pool: mariadb.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_DB,
+        acquireTimeout: 1000
+    })
+}
