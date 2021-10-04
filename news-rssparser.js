c463c915e22af8d00eccc4c4ee1659c62b91871e
serebrs <serebr.s@gmail.com>
2021-10-04 16:29:32 +0600

    заменил rss библиотеку

--- news-rssparser.js
+++ news-rssparser.js
@@ -1,16 +1,17 @@
-const rssParser = require('rss-url-parser');
+const rssParser = require('rss-parser');
 const mariadb = require('mariadb');
 const { localConn, herokuConn } = require('./db');
 
 const rssUrl = 'https://xn--80axf.xn--b1aew.xn--p1ai/Press-sluzhba/Novosti/rss';
 
 exports.getNews = async function () {
+    const parser = new rssParser({ timeout: 1000 });
     let conn;
     try {
         let newsArr = [];
-        conn = await mariadb.createConnection(localConn);
-        const feed = await rssParser(rssUrl);
-        let items = feed.slice(0, 5);
+        conn = await mariadb.createConnection(herokuConn);
+        const feed = await parser.parseURL(rssUrl);
+        let items = feed.items.slice(0, 3);
         items.reverse();
         const [lastRead] = await conn.query("SELECT datetime FROM newsbot LIMIT 1");
         const lastDatetime = lastRead?.datetime || 0;
@@ -18,7 +19,7 @@ exports.getNews = async function () {
         for (let item of items) {
             let pubDate = new Date(item.pubDate).getTime();
             if (lastDatetime < pubDate) {
-                let str = `<b>Новости ОмА МВД России >> <a href="${item.link}">${item.title}</a></b>\n${item.summary || ''}\n<a href="${item.enclosures[0]?.url || ''}">&#8205;</a>`;
+                let str = `<b>Новости ОмА МВД России >> <a href="${item.link}">${item.title} </a></b>\n${item.content}\n<a href="${item?.enclosure?.url}">&#8205;</a>`;
                 newsArr.push(str);
             }
             latestPubDate = Math.max(latestPubDate, pubDate);
