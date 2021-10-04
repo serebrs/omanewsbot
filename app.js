8ca56a3b9cb11fc104b5554f332fd0b2ded13600
serebrs <serebr.s@gmail.com>
2021-10-04 16:54:35 +0600

    job add try-catch

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ const { getWeather } = require('./weather');
 const { getNews } = require('./news');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const chatId = process.env.TELEGRAM_CHAT_ID;
 
 bot.launch();
 
-taskNews = cron.scheduleJob('*/5 * * * *', async () => await jobSend(getNews));
-taskWeather = cron.scheduleJob('*/60 9-19 * * *', async () => await jobSend(getWeather));
+try {
+    taskNews = cron.scheduleJob('*/5 * * * *', async () => await jobSend(getNews));
+    taskWeather = cron.scheduleJob('*/60 9-19 * * *', async () => await jobSend(getWeather));
+}
+catch(err) {
+    console.log(err);
+}
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-const jobSend = async function (funcGetData) {
+async function jobSend(funcGetData) {
     const data = await funcGetData();
     if (!data) return;
     for (let item of data) {
-        await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, item, { parse_mode: "HTML" });
+        await bot.telegram.sendMessage(chatId, item, { parse_mode: "HTML" });
     }
 }
