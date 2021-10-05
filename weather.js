f978698eeff80a0d38ee283d37515e31c8d205ef
serebrs <serebr.s@gmail.com>
2021-10-05 09:22:04 +0600

    axios timeout

--- weather.js
+++ weather.js
@@ -16,7 +16,7 @@ const iconMap = new Map([
 
 exports.getWeather = async function () {
     try {
-        let weatherData = await axios.get(weatherUrl);
+        let weatherData = await axios.get(weatherUrl, {timeout: 1000});
         let iconUni = iconMap.get(weatherData.data.weather[0].icon) || "&#8205;";
         let str = `<b>Погода в Омске сейчас >> </b>\n${weatherData.data.weather[0].description}\n<b>${Math.round(weatherData.data.main.temp)}</b> ℃  ${iconUni}`;
         return [str];
